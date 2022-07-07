d06f511b9b33e95b3baa85df23eb67dd7c062aa6
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-07-07 09:03:41 +0200

    Create CRUD api for library

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BookAPIView
+from .views import BookAPIView, DetailBookAPIView, CreateBookAPIView
 
 
 urlpatterns = [
     path('', BookAPIView.as_view()),
+    path('<int:pk>/', DetailBookAPIView.as_view()),
+    path('create/', CreateBookAPIView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -6,3 +6,13 @@ from .serializers import BookSerializer
 class BookAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class CreateBookAPIView(generics.CreateAPIView):
+    serializer_class = BookSerializer
+
+
+
+class DetailBookAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
